c3d300a098fd3020cfa77c8cdcb447080a578702
kaitlinchoffmann  <kaitlinchoffmann@gmail.com>
2025-06-27 16:44:58 -0400

    Module 4 Part 1

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.h
 
 app.use('/user', userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
